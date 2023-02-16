57ff76c3a352622ebc736353267d2f695d1a5d77
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-02-16 21:33:32 +0100

    iteration 3 completed

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,28 +1,25 @@
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
 function Tweet(props) {
   const { user, timestamp, message } = props.tweet
   return (
     <div className="tweet">
-      <img src={user.image} className="profile" alt="profile" />
+      <ProfileImage imgSrc={user.image}/>
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{user.name}</span>
-            <span className="handle">{user.handle}</span>
-          </span>
+         <User name={user.name} handle={user.handle} />
 
-          <span className="timestamp">{timestamp}</span>
+          <Timestamp timestamp={timestamp} />
         </div>
 
-        <p className="message">{message}</p>
+        <Message message={message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
